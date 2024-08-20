7224f92424387af207c1b50c42d76604c68840ea
Ana Eduarda <anasantoseduarda296@gmail.com>
2024-08-19 22:25:55 -0300

    ELABORANDO UM PROGRAMA

---
+++ valor.js
@@ -0,0 +1,15 @@
+//Elabore um programa que:
+//solicite o nome do produto
+//solicite a quantidade vendida
+//solicite o valor do produto
+//apresente o valor total
+//solicite a % de desconto
+//apresente o R$ total de desconto
+//apresente o valor total da venda depois do desconto
+
+var nomeDoProduto  = parseFloat (prompt("Digite o nome do produto:") );
+var quantidadeVendida = parseInt (prompt("Digite a quantidade vendida: ") );
+var valorDoProduto = parseInt (prompt("Digite o valor do produto: ") );
+var valorTotal = quantidadeVendida * valorDoProduto
+var desconto
+var totalDoDesconto
