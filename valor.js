e06be4d4237978fc41bc1e0d632a24081734fd76
Ana Eduarda <anasantoseduarda296@gmail.com>
2024-08-25 09:38:46 -0300

    Update valor.js

--- valor.js
+++ valor.js
@@ -12,5 +12,15 @@ var quantidadeVendida = parseInt (prompt("Digite a quantidade vendida: ") );
 var valorDoProduto = parsefloat(prompt("Digite o valor do produto: ") );
 var valorTotal = quantidadeVendida * valorDoProduto;
 console.log ("O valor total é: " + " R$ " + valorTotal);
+
+var porcentagemDesconto = parseInt(prompt("Digite a porcentagem do desconto"));
+var totalDesconto = porcentagemDesconto/100;
+var descontoTotal = totalDesconto * valorTotal;
+console.log ("O valor da venda é: " + " R$ " + descontoTotal);
+
+var valorVenda = valorTotal - descontoTotal;
+console.log ("O valor da venda é: " + " R$ " + valorVenda);
+
+
  
 
